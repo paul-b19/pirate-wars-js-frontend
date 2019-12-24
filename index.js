31c8cfe394ab46ac35af7f7006b6f0c3b175fd3e
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-24 16:39:20 -0500

    added grids, design improvemets, name input validation

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const url = "http://localhost:3000/leaders"
+const homeScreen = document.querySelector('#home-screen')
+const gameScreen = document.querySelector('#game-container')
+const quitLink = document.querySelector('#quit-link')
+
+//  navbar collapse
+function collapseNavBar() {
+  $('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+}
 
 //  rendering avatars
 function renderAvatars() {
@@ -21,7 +31,6 @@ function renderAvatars() {
 
 //  rendering leaderbord
 function renderLeaderboard() {
-  console.log('Leaderboard')
   fetch(url).then(resp => resp.json())
     .then(leaders => addLeaders(leaders))
 }
@@ -64,15 +73,53 @@ function renderGameGrids() {
     for (y=0; y<10; y++) {
       let cellP = document.createElement("div")
       cellP.dataset.coordinates = `${x}${y}`
-      cellP.setAttribute("class", "cell col-xs-1 border border-dark")
+      cellP.setAttribute("class", "cell border border-dark")
       rowP.appendChild(cellP)
 
       let cellC = document.createElement("div")
       cellC.dataset.coordinates = `${x}${y}`
-      cellC.setAttribute("class", "cell col-xs-1 border border-dark")
+      cellC.setAttribute("class", "cell border border-dark")
       rowC.appendChild(cellC)
     }
   } 
+}
+
+//  starting a game
+function letsPlay(event) {
+  event.preventDefault();
+  const form = event.target.parentNode
+  console.log(form.elements[0].value)
+
+  const nameInput = document.querySelector('#playerName')
+  const errMsg = document.querySelector('.invalid-feedback')
+
+  if (form.elements[0].value === "") {
+    nameInput.classList.add("is-invalid")
+    // alert("Please provide a username!")
+    nameInput.classList.add("apply-shake");
+    nameInput.addEventListener("animationend", (e) => {
+      nameInput.classList.remove("apply-shake");
+    })
+    errMsg.setAttribute("style", "display: initial;")
+
+  } else {
+    nameInput.classList.remove("is-invalid")
+    errMsg.setAttribute("style", "display: none;")
+    form.reset()
+    homeScreen.setAttribute("style", "display: none;")
+    // gameScreen.setAttribute("style", "display: block;")
+    gameScreen.style.display = ""
+    quitLink.setAttribute("style", "display: initial;")
+  }
+}
+
+//  quitting game
+function quitGame(event) {
+  event.preventDefault()
+  console.log(event.target)
+  gameScreen.setAttribute("style", "display: none;")
+  quitLink.setAttribute("style", "display: none;")
+  homeScreen.setAttribute("style", "display: initial;")
 }
 
 
@@ -87,7 +134,9 @@ function renderGameGrids() {
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('DOM fully loaded and parsed');
+  collapseNavBar()
   renderAvatars()
   renderLeaderboard()
   renderGameGrids()
+
 });
