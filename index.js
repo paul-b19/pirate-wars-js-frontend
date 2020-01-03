542818220a2a3b77647f3a9912420b58f00a0eb8
Pavel Beletsky <pavel.beletsky@live.com>
2020-01-03 12:35:19 -0500

    heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = "http://pirate-wars.herokuapp.com/leaders"
+const url = "http://pirate-wars-rails.herokuapp.com/leaders"
 const audio = document.querySelector("audio")
 const prBar = document.querySelector('.while-loading')
 const leaderbord = document.querySelector('tbody')
